d44d0491532c838a4b0498c79a1da998b5621b15
Carter Rogers <tavanthlee@outlook.com>
2026-07-01 14:11:09 -0400

    Bug fixes

--- server.js
+++ server.js
@@ -44,12 +44,8 @@ function isValidUrl(value) {
 
 app.post("/shorten", limiter, async (req, res) => {
   const longUrl = req.body.longUrl;
-  try {
-    const url = new URL(longUrl);
-    if (!isValidUrl(longUrl)) {
-      return res.status(400).json({ error: "Invalid URL" });
-    }
-  } catch {
+
+  if (!isValidUrl(longUrl)) {
     return res.status(400).json({ error: "Invalid URL" });
   }
   try {
@@ -93,9 +89,9 @@ app.get("/:code", async (req, res) => {
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "URL not found" });
     }
-    const longUrl = result.rows[0].longUrl;
+    const longUrl = result.rows[0].long_url;
     if (!isValidUrl(longUrl)) {
-      return res.status(500).json({ error: "Stored URL is not valid " });
+      return res.status(500).json({ error: "Stored URL is invalid" });
     }
     res.redirect(longUrl);
   } catch (err) {
